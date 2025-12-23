5a68b917035a8f6d95e36edeeed6567f01afd5a4
PrashantM-9803 <PrashantMukund.Mahamuni@cognizant.com>
2025-12-23 12:20:33 +0530

    fix: lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 export const routes: Routes = [
     {
         path:"",
+        // laxy loading
         // loadComponent: () => import('./hero-section/hero-section.component').then(m => m.HeroSectionComponent)
         component: HeroSectionComponent
     },
     {
         path: 'contact',
+        // lazy loading
         // loadComponent: () => import('./contact-us/contact-us.component').then(m => m.ContactUsComponent)
         component: ContactUsComponent
     }
