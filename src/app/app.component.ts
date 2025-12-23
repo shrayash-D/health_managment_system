9bb90661e8006ce75fb4ddadbdda6edbb8a73048
Prashant Mahamuni <prashantmahamuni9803@gmail.com>
2025-12-23 11:27:53 +0530

    pages: created navbar, heroSection, footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { ContactUsComponent } from "./contact-us/contact-us.component";
 import { DiagnosisFormComponent } from "./diagnosis-form/diagnosis-form.component";
 import { PrescriptionFormComponent } from "./prescription-form/prescription-form.component";
 import { PatientDashboardComponent } from "./patient-dashboard/patient-dashboard.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactUsComponent, DiagnosisFormComponent, PrescriptionFormComponent, PatientDashboardComponent],
+  imports: [RouterOutlet, ContactUsComponent, DiagnosisFormComponent, PrescriptionFormComponent, PatientDashboardComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        loadComponent: () => import('./hero-section/hero-section.component').then(m => m.HeroSectionComponent)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./contact-us/contact-us.component').then(m => m.ContactUsComponent)
+    }
+];
