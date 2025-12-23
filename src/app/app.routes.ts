fb988451de27ef774e4468cbc3975cb6993b31ec
PrashantM-9803 <PrashantMukund.Mahamuni@cognizant.com>
2025-12-23 12:18:31 +0530

    fix: updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 export const routes: Routes = [
     {
         path:"",
-        loadComponent: () => import('./hero-section/hero-section.component').then(m => m.HeroSectionComponent)
+        // loadComponent: () => import('./hero-section/hero-section.component').then(m => m.HeroSectionComponent)
+        component: HeroSectionComponent
     },
     {
         path: 'contact',
-        loadComponent: () => import('./contact-us/contact-us.component').then(m => m.ContactUsComponent)
+        // loadComponent: () => import('./contact-us/contact-us.component').then(m => m.ContactUsComponent)
+        component: ContactUsComponent
     }
 ];
